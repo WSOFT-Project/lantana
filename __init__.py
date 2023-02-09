551213228fb5f742036b3e317acfac1b2402bf49
taiseiue <taisei@wsoft.gq>
2023-02-09 18:07:39 +0900

    Lantana v2.3.0

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 __copyright__    = 'Copyright (C) 2022 WSOFT.'
-__version__      = '2.2.0'
+__version__      = '2.3.0'
 __license__      = 'MIT'
 __author__       = 'WSOFT'
 __author_email__ = 'info@wsoft.ws'
