547907b6f4f8886a56be7e14547ef559a97a88f8
taiseiue <taisei@wsoft.ws>
2023-06-30 22:43:43 +0900

    v2.7.5

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 __copyright__    = 'Copyright (C) 2022 WSOFT.'
-__version__      = '2.7.3'
+__version__      = '2.7.5'
 __license__      = 'MIT'
 __author__       = 'WSOFT'
 __author_email__ = 'info@wsoft.ws'
