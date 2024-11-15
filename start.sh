c51d4359fa50fbc986a95bc32c39086560c52a97
taiseiue <taisei@wsoft.ws>
2024-11-16 03:34:40 +0900

    英語対応

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+python3 -m venv venv
 source venv/bin/activate
+pip3 install 'mkdocs[i18n]'
+
+# Building i18n support
+pybabel compile --statistics --directory lantana/locales -l en
+pybabel compile --statistics --directory lantana/locales -l ja
+
 pip3 install -e .
 mkdocs serve --watch-theme
