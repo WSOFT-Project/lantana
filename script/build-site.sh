cb100ebe5ee672675dcd8f37ba92ab9fba936f2a
taiseiue <taisei@wsoft.ws>
2024-11-17 23:49:51 +0900

    mkdocs-static-i18n対応を実装

---
+++ script/build-site.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install 'mkdocs[i18n]'
+
+# Building i18n support
+pybabel compile --statistics --directory lantana/locales -l en
+pybabel compile --statistics --directory lantana/locales -l ja
+
+# Install this package
+pip install .
+
+# And i18n support for site
+pip install mkdocs-static-i18n==1.2.3
+
+# Build site
+mkdocs build
